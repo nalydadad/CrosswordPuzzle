464a2d0c0512c9f470d38ed0abc4c99746d9cc8a
Mac <nalydadad@gmail.com>
2016-12-05 14:04:31 +0800

    Initial Commit

---
+++ DDCrosswordPuzzle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DDCrosswordPuzzle
+//
+//  Created by DADA on 2016/12/5.
+//  Copyright © 2016年 DADA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
